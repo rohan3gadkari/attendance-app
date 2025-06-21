43f71a4d90627edadd1d16ea966e205cf59dcef2
rohan3gadkari <gadkarirohan2004@gmail.com>
2025-06-21 12:38:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// LOGIN
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(error => alert("Login Error: " + error.message));
+}
+
+// REGISTER
 function register() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
+    .then(() => {
       alert("Registered successfully!");
     })
-    .catch((error) => {
-      alert("Registration error: " + error.message);
-    });
+    .catch(error => alert("Registration Error: " + error.message));
 }
